c747a5efb3d3d7070d17540ec19827402dcc0ee1
amalbabus <amalabus33@gmail.com>
2021-11-28 15:51:50 +0530

    script ready for test

--- git-import/script.py
+++ git-import/script.py
@@ -4,7 +4,10 @@ import commands
 repoName =glob.glob("*")
 
 for repo in repoName:
-    os.system('ghe-migrator prepare migration_archive_cdsl4linux.tar.gz > file.txt')   
+    if(repo =="script.py"):
+        continue
+    print("prepare started for ",repo)
+    os.system('ghe-migrator prepare'+repo+' > file.txt')   
     status,guid = commands.getstatusoutput("grep -i guid file.txt | awk '{print $3}'")
     os.system('ghe-migrator conflicts -g'+guid+' > conflicts.csv')  
     os.system('ghe-migrator map merge -i conflicts.csv -g'+guid)   
