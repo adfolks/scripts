19509643580ea64370e9dc8884c2e2d7f017d0a4
amalbabus <amalabus33@gmail.com>
2021-11-28 12:45:27 +0530

    testing

--- git-import/script.py
+++ git-import/script.py
@@ -2,6 +2,9 @@ import glob
 import os
 repoName =glob.glob("*")
 
-for repo in repoName:
-    os.system('ghe-migrator prepare '+repo)
-    
+# for repo in repoName:
+#     # os.system('ghe-migrator prepare '+repo)
+#     os.system('cat '+repo+' > file.txt')
+
+
+os.system('ghe-migrator prepare migration_archive_cdsl4linux.tar.gz > file.txt')    
