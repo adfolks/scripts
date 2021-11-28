c4066991638afb20e92c823fcecfb1a8e4d0d21e
amalbabus <amalabus33@gmail.com>
2021-11-28 16:45:03 +0530

    script ready for test

--- git-import/script.py
+++ git-import/script.py
@@ -8,14 +8,14 @@ for repo in repoName:
     if(repo =="script.py"):
         continue
     print("Preparation started for ",repo)
-    os.system('ghe-migrator prepare'+repo+' > file.txt')   
+    os.system('ghe-migrator prepare '+repo+' > file.txt')   
     status,guid = sp.getstatusoutput("grep -i guid file.txt | awk '{print $3}'")
     os.system('ghe-migrator conflicts -g'+guid+' > conflicts.csv')  
     os.system('ghe-migrator map merge -i conflicts.csv -g'+guid)   
     os.system('ghe-migrator conflicts -g'+guid+' > conflicts.csv')  
     os.system('ghe-migrator map merge -i conflicts.csv -g'+guid)   
     print("Importing repo ",repo)
-    os.system('ghe-migrator import '+repo+' -g'+guid+'-u ssrcdevops -p' )
+    os.system('ghe-migrator import '+repo+' -g'+guid+' -u ssrcdevops -p' )
     print("Unlocking repo ",repo)
     os.system('ghe-migrator unlock -g'+guid)
     os.system('echo'+repo+' '+guid+'>> guidfile.txt')
